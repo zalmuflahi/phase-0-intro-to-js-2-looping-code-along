249bd7ce5306eb4e8bce2aaaf688e78a8e09a5ce
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-17 16:47:30 -0400

    commit tree c36194e9c294cb4c23d4da3af62e58916bd8c5f0

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(cards, x) {
+   var y = [];
+    for (var i = 0; i < cards.length ; i++) {
+            y.push(`Thank you, ${cards[i]}, for the wonderful ${x} gift!` );
+     }
+    return y;
+    }
+function countDown(){
+    var countdown = 11;
+while (countdown > 0){
+countdown--;
+console. log(countdown);
+}
+
+}
